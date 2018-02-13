093b12505dcbcaab9cfe5796b6fce6cdd238d236
power <power_android@163.com>
2018-02-13 16:25:19 +0800

    POWER

---
+++ app/src/main/java/com/power/customizingthecloud/activity/mine/RengyangDetailActivity.java
@@ -0,0 +1,16 @@
+package com.power.customizingthecloud.activity.mine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.power.customizingthecloud.R;
+import com.power.customizingthecloud.base.BaseActivity;
+
+public class RengyangDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rengyang_detail);
+    }
+}
