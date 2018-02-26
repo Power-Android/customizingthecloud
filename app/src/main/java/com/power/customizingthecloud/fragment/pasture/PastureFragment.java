fad5bdcdcfd46965154f261a5e4120cbca37713e
pengchengla@126.com <1234QWERasdf>
2018-02-26 10:38:23 +0800

    刘新魁

--- app/src/main/java/com/power/customizingthecloud/fragment/pasture/PastureFragment.java
+++ app/src/main/java/com/power/customizingthecloud/fragment/pasture/PastureFragment.java
@@ -1,27 +1,75 @@
 package com.power.customizingthecloud.fragment.pasture;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.power.customizingthecloud.R;
 import com.power.customizingthecloud.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Administrator on 2018/1/19.
  * 牧场
  */
 
-public class PastureFragment extends BaseFragment {
+public class PastureFragment extends BaseFragment{
+    @BindView(R.id.title_message_iv)
+    ImageView mTitleMessageIv;
+    @BindView(R.id.title_back_iv)
+    ImageView mTitleBackIv;
+    @BindView(R.id.title_list_iv)
+    ImageView mTitleListIv;
+    @BindView(R.id.title_content_tv)
+    TextView mTitleContentTv;
+    @BindView(R.id.title_sign_in_iv)
+    ImageView mTitleSignInIv;
+    @BindView(R.id.title_setting_iv)
+    ImageView mTitleSettingIv;
+    @BindView(R.id.title_qrcode_iv)
+    ImageView mTitleQrcodeIv;
+    @BindView(R.id.title_shopcar_iv)
+    ImageView mTitleShopcarIv;
+    @BindView(R.id.title_share_iv)
+    ImageView mTitleShareIv;
+    @BindView(R.id.title_search_iv)
+    ImageView mTitleSearchIv;
+    @BindView(R.id.title_jia_iv)
+    ImageView mTitleJiaIv;
+    @BindView(R.id.title_kefu_iv)
+    ImageView mTitleKefuIv;
+    @BindView(R.id.title_content_right_tv)
+    TextView mTitleContentRightTv;
+    Unbinder unbinder;
+
     @Override
     protected View initView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pasture, null);
+        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     protected void initLazyData() {
 
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mTitleContentTv.setText("云端牧场");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
