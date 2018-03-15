76b23bedf5def453ece2cd3698861d08ec63cb68
power <power_android@163.com>
2018-03-15 10:50:25 +0800

    POWER

--- app/src/main/java/com/power/customizingthecloud/activity/mine/DonkeyEarsDetailActivity.java
+++ app/src/main/java/com/power/customizingthecloud/activity/mine/DonkeyEarsDetailActivity.java
@@ -11,9 +11,15 @@ import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.model.HttpHeaders;
+import com.lzy.okgo.model.Response;
 import com.power.customizingthecloud.R;
 import com.power.customizingthecloud.base.BaseActivity;
 import com.power.customizingthecloud.bean.DonkeyEarsDetailBaen;
+import com.power.customizingthecloud.callback.DialogCallback;
+import com.power.customizingthecloud.utils.SpUtils;
+import com.power.customizingthecloud.utils.Urls;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +36,7 @@ public class DonkeyEarsDetailActivity extends BaseActivity {
     TextView titleContentTv;
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
+    private List<DonkeyEarsDetailBaen.DataBean> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,27 +53,39 @@ public class DonkeyEarsDetailActivity extends BaseActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
         recyclerView.setNestedScrollingEnabled(false);
 
-        List<DonkeyEarsDetailBaen> list = new ArrayList<>();
-        for (int i = 0; i <4; i++) {
-            DonkeyEarsDetailBaen detailBaen = new DonkeyEarsDetailBaen();
-            detailBaen.setDate("2017.02.1"+ i);
-            detailBaen.setContent("您今天签到获得平台奖励一只驴耳朵");
-            list.add(detailBaen);
-        }
-        DonkeyEarsAdapter adapter = new DonkeyEarsAdapter(R.layout.item_donkeyears_detail,list);
-        recyclerView.setAdapter(adapter);
+        initData();
     }
 
-    private class DonkeyEarsAdapter extends BaseQuickAdapter<DonkeyEarsDetailBaen,BaseViewHolder>{
+    private void initData() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.put("Authorization", "Bearer " + SpUtils.getString(mContext, "token", ""));
 
-        public DonkeyEarsAdapter(@LayoutRes int layoutResId, @Nullable List<DonkeyEarsDetailBaen> data) {
+        OkGo.<DonkeyEarsDetailBaen>get(Urls.BASEURL + "api/v2/eselsohr")
+                .tag(this)
+                .headers(headers)
+                .execute(new DialogCallback<DonkeyEarsDetailBaen>(this,DonkeyEarsDetailBaen.class) {
+                    @Override
+                    public void onSuccess(Response<DonkeyEarsDetailBaen> response) {
+                        DonkeyEarsDetailBaen body = response.body();
+                        if (body.getCode() == 1){
+                            list = body.getData();
+                            DonkeyEarsAdapter adapter = new DonkeyEarsAdapter(R.layout.item_donkeyears_detail, list);
+                            recyclerView.setAdapter(adapter);
+                        }
+                    }
+                });
+    }
+
+    private class DonkeyEarsAdapter extends BaseQuickAdapter<DonkeyEarsDetailBaen.DataBean,BaseViewHolder>{
+
+        public DonkeyEarsAdapter(@LayoutRes int layoutResId, @Nullable List<DonkeyEarsDetailBaen.DataBean> data) {
             super(layoutResId, data);
         }
 
         @Override
-        protected void convert(BaseViewHolder helper, DonkeyEarsDetailBaen item) {
-            helper.setText(R.id.item_date_tv,item.getDate())
-                    .setText(R.id.item_content_tv,item.getContent());
+        protected void convert(BaseViewHolder helper, DonkeyEarsDetailBaen.DataBean item) {
+            helper.setText(R.id.item_date_tv,item.getTime())
+                    .setText(R.id.item_content_tv,item.getNote());
         }
     }
 
--- app/src/main/java/com/power/customizingthecloud/bean/DonkeyEarsDetailBaen.java
+++ app/src/main/java/com/power/customizingthecloud/bean/DonkeyEarsDetailBaen.java
@@ -1,28 +1,101 @@
 package com.power.customizingthecloud.bean;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/2/13.
  */
 
 public class DonkeyEarsDetailBaen implements Serializable {
-    private String date;
-    private String content;
 
-    public String getDate() {
-        return date;
+    /**
+     * code : 1
+     * message : ok
+     * data : [{"id":4,"user_id":1,"number":1,"note":"签到获得1驴耳朵","time":"2018.02.26"},{"id":3,"user_id":1,"number":10,"note":"购买商品抵扣10驴耳朵[订单号：3]","time":"2018.02.24"},{"id":1,"user_id":1,"number":50,"note":"新用户注册，填写您为邀请人获取50驴耳朵","time":"2018.01.19"}]
+     */
+
+    private int code;
+    private String message;
+    private List<DataBean> data;
+
+    public int getCode() {
+        return code;
     }
 
-    public void setDate(String date) {
-        this.date = date;
+    public void setCode(int code) {
+        this.code = code;
     }
 
-    public String getContent() {
-        return content;
+    public String getMessage() {
+        return message;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<DataBean> getData() {
+        return data;
+    }
+
+    public void setData(List<DataBean> data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        /**
+         * id : 4
+         * user_id : 1
+         * number : 1
+         * note : 签到获得1驴耳朵
+         * time : 2018.02.26
+         */
+
+        private int id;
+        private int user_id;
+        private int number;
+        private String note;
+        private String time;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getUser_id() {
+            return user_id;
+        }
+
+        public void setUser_id(int user_id) {
+            this.user_id = user_id;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public String getNote() {
+            return note;
+        }
+
+        public void setNote(String note) {
+            this.note = note;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
